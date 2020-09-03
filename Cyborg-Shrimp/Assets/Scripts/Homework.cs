3e5ce74318259fc2e78d545662b5b851dcba3914
Samantha Haddock <10911252@my.uvu.edu>
2020-09-03 17:53:02 -0400

    Homework: create 4 variables

---
+++ Cyborg-Shrimp/Assets/Scripts/Homework.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+In a single script write variables for the following:
+
+Health as a float
+Password as a string
+Player name as a string
+Points as an int
+ */
+public class Homework : MonoBehaviour
+{
+    public float health = 100; 
+    public string password = ("password"); 
+    public string playerName = ("link"); 
+    public int points = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
