f7a6e24d59a517cdbe9cb8fb53e46ded6908b793
Samantha Haddock <10911252@my.uvu.edu>
2020-09-11 13:48:17 -0600

    Homework: write a 2nd powerup
    added "ManaPowerUp" script as well as sphere asset

---
+++ Cyborg-Shrimp/Assets/Scripts/Powerups/ManaPowerUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaPowerUp : MonoBehaviour
+{
+    public int mana;
+    private void OnTriggerEnter(Collider other)
+    {
+        mana++;
+        print(mana);
+    }
+}
