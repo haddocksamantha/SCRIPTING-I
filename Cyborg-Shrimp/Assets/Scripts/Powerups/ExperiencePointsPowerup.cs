5ed0d72ef847067464f8b1ce007d833bdacd00c1
Samantha Haddock <10911252@my.uvu.edu>
2020-09-12 10:52:10 -0600

    Homework: write a 3rd powerup
    "XP Powerup" added

---
+++ Cyborg-Shrimp/Assets/Scripts/Powerups/ExperiencePointsPowerup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperiencePointsPowerup : MonoBehaviour
+{
+    public int XP;
+    private void OnTriggerEnter(Collider other)
+    {
+        XP++;
+        print(XP);
+    }
+}
