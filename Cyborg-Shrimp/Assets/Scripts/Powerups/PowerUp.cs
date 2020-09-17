5ee457f45f05709b14e4afe36dfaae7cce2fe244
Samantha Haddock <10911252@my.uvu.edu>
2020-09-17 15:11:39 -0600

    lecture 9/17

--- Cyborg-Shrimp/Assets/Scripts/Powerups/PowerUp.cs
+++ Cyborg-Shrimp/Assets/Scripts/Powerups/PowerUp.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
+    public UnityEvent powerUpEvent;
+    
     private void OnTriggerEnter(Collider other)
     {
         score++;
         print(score);
+        powerUpEvent.Invoke();
     }
 }
