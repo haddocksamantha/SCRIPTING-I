18e3c43c46b28ff087a39b985cc1538d98307020
Samantha Haddock <10911252@my.uvu.edu>
2020-09-23 17:36:38 -0400

    CWC 2.2

--- Create With Code - Sam H/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create With Code - Sam H/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,9 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 15;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject projectilePrefab;
+    
+    
     // Update is called once per frame
     void Update()
     {
@@ -28,5 +25,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
