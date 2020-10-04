e01f533ee7fe09fdb198b8ac228a192c39510a4d
Samantha Haddock <10911252@my.uvu.edu>
2020-10-04 14:41:24 -0400

    CWC 4.1

---
+++ Create With Code - Sam H/Prototype 4/Assets/Scripts/PlayerConroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConroller : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = gameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
