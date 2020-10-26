b4a5523ccb298d0435cedb7b132a177e3ae4028e
Samantha Haddock <10911252@my.uvu.edu>
2020-10-26 15:20:21 -0400

    added scriptable objcet

---
+++ Cyborg-Shrimp/Assets/Scripts/ScriptableObjects/SprayCans.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu]
+public class SprayCans : ScriptableObject
+{
+    public bool pickedUp = false;
+    public int numberOfCans;
+    public string sprayCanColor;
+    
+    public void UpdateNumberOfCans(int number)
+    {
+        numberOfCans += number;
+    }
+    
+    public void ReplaceNumberOfCans(int number)
+    {
+       numberOfCans = number;
+    } 
+
+    public void DisplayImage(Image img)
+    {
+        img.fillAmount = numberOfCans;
+        sprayCanColor = "Full";
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = numberOfCans.ToString();
+    }
+
+
+}
