1ef1dde743b905602b275c0328ea6ce7ff6542d1
Samantha Haddock <10911252@my.uvu.edu>
2020-10-27 13:35:49 -0400

    homework 10.27

---
+++ Cyborg-Shrimp/Assets/Scripts/ScriptableObjects/PlayerInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerInfo : ScriptableObject
+{
+    public string name;
+    public string gender;
+    public string nationality;
+    public int age;
+    public float height;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
