f55e897430c156074d3553e763792418143f22d2
Samantha Haddock <10911252@my.uvu.edu>
2020-10-27 16:46:59 -0400

    class 10.27

---
+++ Cyborg-Shrimp/Assets/Scripts/ScriptableObjects/Collections.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collections : ScriptableObject
+{
+    public List<string> collectionList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(collectionList)
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
