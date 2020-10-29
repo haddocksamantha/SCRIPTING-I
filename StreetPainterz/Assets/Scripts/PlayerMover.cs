9af146df75444f48c37c18cb51c1d4b05354251a
Samantha Haddock <10911252@my.uvu.edu>
2020-10-29 19:25:00 -0400

    started scripting new charactermover

---
+++ StreetPainterz/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody playerRigidbody;
+    private Vector3 moveDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(moveDirection * speed * Time.deltaTime);
+        }
+    }
+}
