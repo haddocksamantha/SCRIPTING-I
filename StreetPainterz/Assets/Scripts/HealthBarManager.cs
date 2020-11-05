cd654a8204592a4f77899ae3c9084c51f17e7fe3
Samantha Haddock <10911252@my.uvu.edu>
2020-11-05 18:38:59 -0500

    added healthbar

---
+++ StreetPainterz/Assets/Scripts/HealthBarManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  HealthBarManager : MonoBehaviour
+{
+    private GameObject healthBar;
+
+    public int playerHealth = 100;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeHealth();
+    }
+
+    void StartHealth()
+    {
+        healthBar = GameObject.FindWithTag("HealthBar");
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+    void ChangeHealth()
+    {
+        //add code to subtract health if player collides with enemy
+    }
+}
