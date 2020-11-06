e6575a909dbb50195a069bffb5b4d0b0d55c6e75
Samantha Haddock <10911252@my.uvu.edu>
2020-11-06 18:09:26 -0500

    homework

---
+++ StreetPainterz/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class LightSwitch : MonoBehaviour
+{
+    public bool playerOn = false;
+    public GameObject pinkLight;
+
+    void Start()
+    {
+        pinkLight = GameObject.Find("Pink Light");
+        pinkLight.SetActive(false);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (GameObject.FindWithTag("Player"))
+        {
+            playerOn = true;
+            pinkLight.SetActive(true);
+            Debug.Log("Light On");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (GameObject.FindWithTag("Player"))
+        { 
+            playerOn = false;
+           pinkLight.SetActive(false);
+        }
+    }
+}
