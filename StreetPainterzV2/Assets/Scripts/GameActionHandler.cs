56909031da4fbba9ebcbab89ba958b6be837bf4b
Samantha Haddock <10911252@my.uvu.edu>
2020-11-10 23:24:12 -0500

    scripted weapons switch

---
+++ StreetPainterzV2/Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering.VirtualTexturing;
+/*
+public class GameActionHandler : MonoBehaviour
+{
+    public UnityAction actionObj;
+    public UnityEvent handlerEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        actionObj.action += Handle;
+    }
+
+    private void Handle()
+    {
+        handlerEvent.Invoke();
+    }
+}
+*/
---
+++ StreetPainterzV2/Assets/Scripts/ScriptableObjects/Weapons.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Weapons : ScriptableObject
+{
+  public int weapon = 5;
+
+  private string noWeapon;
+
+  void Start()
+  {
+    noWeapon = "No Weapon Selected";
+  }
+  
+  void WeaponsSwitch()
+  {
+    switch (weapon)
+    {
+      case 5:
+        GameObject.FindWithTag("Sword");
+        break;
+      case 4:
+        GameObject.FindWithTag("Gun");
+        break;
+      case 3:
+        GameObject.FindWithTag("Knife");
+        break;
+      case 2:
+        GameObject.FindWithTag("Katana");
+        break;
+      case 1:
+        GameObject.FindWithTag("Bomb");
+        break;
+      default:
+        Debug.Log(noWeapon);
+        break;
+    }
+  }
+ 
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+/*
+ public GameObject Shield;
+  public GameObject Potions;
+  public GameObject ExtraHealth;
+  public GameObject Buffs;
+  public GameObject SuperShield;
+  public enum Defenses()
+  {
+    Shield,
+    Potions,
+    ExtraHealth,
+    Buffs,
+    SuperShield
+  }
+
+  void Start()
+  {
+    Shield = GetComponent<GameObject>();
+    Potions = GetComponent<GameObject>();
+    ExtraHealth = GetComponent<GameObject>();
+    Buffs = GetComponent<GameObject>();
+    SuperShield = GetComponent<GameObject>();
+
+  }
+
+  public Defenses currentDefence;
+
+  public void RunCurrentDefense()
+  {
+    switch (currentDefence)
+    {
+      case Defenses.Shield;
+        break;
+      case Defenses.Potions ;
+        break;
+      case Defenses.ExtraHealth ;
+        break;
+      case Defenses.Buffs ;
+        break;
+      case Defenses.SuperShield ;
+        break;
+    }
+  }
+  */
