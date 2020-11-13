275b8d9583ac250dc43b95bfe807a6240465dc38
Samantha Haddock <10911252@my.uvu.edu>
2020-11-12 22:27:41 -0500

    AI homework

---
+++ StreetPainterzV2/Assets/Scripts/AIBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehavior : MonoBehaviour
+{
+   private NavMeshAgent agent;
+   public Transform player;
+
+   private void Start()
+   {
+       agent = GetComponent<NavMeshAgent>();
+   }
+
+   private void Update()
+   {
+       agent.destination = player.position;
+   }
+}
