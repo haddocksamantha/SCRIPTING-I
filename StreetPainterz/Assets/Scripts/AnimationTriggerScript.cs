496c7854eaa844f04ae080a97ea7321eed735cca
Samantha Haddock <10911252@my.uvu.edu>
2020-12-02 17:16:44 -0500

    worked on animation

---
+++ StreetPainterz/Assets/Scripts/AnimationTriggerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTriggerScript : MonoBehaviour
+{
+    public Animator animationEx;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationEx = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      AnimationKeyPress();  
+    }
+
+    void AnimationKeyPress()
+    {
+        if(Input.GetKeyDown("A"))
+        {
+            animationEx.Play("AnimationExample");
+        }
+    }
+}
