3fc7afba874e944a002cf14a430ad8731ad7559c
Samantha Haddock <10911252@my.uvu.edu>
2020-12-04 00:17:45 -0500

    more animation

---
+++ StreetPainterzV2/Assets/Scripts/FlashingLightScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLightScript : MonoBehaviour
+{
+   public Animator lightFlashing;
+
+   void Start()
+   {
+      lightFlashing = GetComponent<Animator>();
+   }
+
+   void Update()
+   {
+      LightKey();
+   }
+
+   void LightKey()
+   {
+      if (Input.GetKeyDown("L"))
+      {
+         lightFlashing.Play("FlashingLight");
+      }
+   }
+   
+}
---
+++ StreetPainterzV2/Assets/Scripts/PlayerScripts/PlayerAnimationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public Animator playerWalkAnim;
+
+    private void Start()
+    {
+        playerWalkAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        PlayerWalkAnimation();
+    }
+
+    void PlayerWalkAnimation()
+    {
+        if (Input.GetKeyDown("Horizontal"))
+        {
+            playerWalkAnim.Play("PlayerWalkAnim");
+        }else if (Input.GetKeyUp("Horizontal"))
+        {
+            playerWalkAnim.Play("PlayerIdleAnim");
+        }
+    }
+}
