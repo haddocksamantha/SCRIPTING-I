fae7d40d6180431159e03b213602b901c4b97285
Samantha Haddock <10911252@my.uvu.edu>
2020-12-05 15:23:17 -0500

    added animation

---
+++ StreetPainterzV2/Assets/Scripts/BoxAnimationScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxAnimationScript : MonoBehaviour
+{
+    public Animator boxAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxAnimation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("B"))
+        {
+            boxAnimation.Play("BoxAnimation");
+        }
+    }
+}
