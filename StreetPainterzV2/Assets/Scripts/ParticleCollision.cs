12d2fee0f60b5f02986700162beb596f8a36e72c
Samantha Haddock <10911252@my.uvu.edu>
2020-12-09 15:09:58 -0500

    added particle system

---
+++ StreetPainterzV2/Assets/Scripts/ParticleCollision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollision : MonoBehaviour
+{
+    public ParticleSystem glitter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        glitter.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        glitter.GetComponent<ParticleSystem>().enableEmission = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        glitter.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+}
